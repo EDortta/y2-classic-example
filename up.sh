59567cefc3d0794dff9a167a3d57cc7530e930be
Esteban D.Dortta <dortta@yahoo.com>
2024-08-19 14:26:46 -0300

    Correction in up.sh to use docker-compose or docker compose

--- up.sh
+++ up.sh
@@ -6,4 +6,12 @@ else
     recipe="docker-compose.yml"
 fi
 
-docker-compose --file $recipe up -d
+dc=`which docker-compose` > /dev/null 2>&1
+if [ -z $dc ]; then
+    dc="docker compose"
+fi
+
+cmd="$dc --file $recipe up -d $1"
+echo "CMD = $cmd"
+
+$cmd
